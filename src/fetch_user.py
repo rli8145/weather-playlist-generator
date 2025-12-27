6d925981a06ca187bbb89996214dc82cc0c7998d
rli8145 <rli8145@gmail.com>
2025-12-26 16:09:11 -0800

    Reorganize user interaction files

--- src/fetch_user.py
+++ src/fetch_user.py
@@ -1,16 +1,26 @@
 import os
 from dotenv import load_dotenv
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-from spotify_auth import get_user_spotify_client
+from spotipy.oauth2 import SpotifyOAuth
 from datetime import date
+
+load_dotenv()
+
+def get_user_spotify_client():
+    return spotipy.Spotify(
+        auth_manager=SpotifyOAuth(
+            client_id=os.getenv("SPOTIFY_CLIENT_ID"),
+            client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
+            redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"),
+            scope="user-top-read playlist-modify-public playlist-modify-private",
+            cache_path=".spotify_cache"
+        )
+    )
 
 sp = get_user_spotify_client()
 user_id = sp.current_user()["id"]
 top_tracks = sp.current_user_top_tracks(limit=50, time_range="medium_term")
 track_ids = [item["id"] for item in top_tracks["items"]]
-
-
 
 #if option pressed
 username = sp.current_user()["display_name"]

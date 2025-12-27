99020b2bfb47b7df8369b49aa6ac64c2665c393a
rli8145 <rli8145@gmail.com>
2025-12-26 16:59:57 -0800

    built fetch_weather and integrated openweathermap api

--- src/fetch_weather.py
+++ src/fetch_weather.py
@@ -1,1 +1,21 @@
+import os
+import requests
+
 #temperature, precipitation, cloud cover
+
+def fetch_by_coords(lat, lon):
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "appid": os.getenv("OPENWEATHER_API_KEY"),
+        "units": "metric"
+    }
+    return requests.get(url, params=params).json()
+
+def fetch_weather(data):
+    return {
+        "temperature": data["main"]["temp"],
+        "precipitation": data.get("rain", {}).get("1h", 0),
+        "cloud_cover": data["clouds"]["all"]
+    }

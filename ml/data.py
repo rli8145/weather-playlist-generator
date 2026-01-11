24f774328a8875feb1769d3860769fe7cbddefae
rli8145 <rli8145@gmail.com>
2026-01-11 14:46:14 -0500

    packaged loading data into function

---
+++ ml/data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+features = ["energy", "valence", "tempo", "acousticness", "loudness"]
+
+def load_data(path: str = "data/track_data.csv"):
+    df = pd.read_csv(path)
+    X = df[features]
+    y = df["weather"]
+    return X, y

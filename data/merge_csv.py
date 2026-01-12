77e702a1090ff7a26adb3419a43b2f132f02ecbf
rli8145 <rli8145@gmail.com>
2026-01-11 19:00:36 -0500

    relabelled snow to snowy in data

--- data/merge_csv.py
+++ data/merge_csv.py
@@ -2,11 +2,11 @@ import pandas as pd
 
 chat = pd.read_csv("data/chat.csv")
 claude = pd.read_csv("data/claude.csv")
-ryan = pd.read_csv("data/ryan.csv")
+# ryan = pd.read_csv("data/ryan.csv")
 
 claude = claude[["weather"]+[c for c in claude.columns if c != "weather"]]
 
-merged = pd.concat([chat, claude, ryan], axis=0, ignore_index=True)
-merged = merged.replace({"snowy": "snow"})
+merged = pd.concat([chat, claude], axis=0, ignore_index=True)
+merged = merged.replace({"snow": "snowy"})
 
 merged.to_csv("data/track_data.csv", index=False)
